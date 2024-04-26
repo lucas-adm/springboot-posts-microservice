faa74a33a04ca2a04ea89e9c98b7e879b8437a09
Lucas Aguiar <vintesetelucas@gmail.com>
2024-04-26 18:04:35 -0300

    🧬 ✨ feat: changing plain text to html message

--- src/main/java/com/adm/lucas/Messaging/adapter/inbound/service/EmailService.java
+++ src/main/java/com/adm/lucas/Messaging/adapter/inbound/service/EmailService.java
@@ -3,12 +3,14 @@ package com.adm.lucas.Messaging.adapter.inbound.service;
 import com.adm.lucas.Messaging.adapter.inbound.entity.EmailEntity;
 import com.adm.lucas.Messaging.adapter.inbound.entity.enums.Status;
 import com.adm.lucas.Messaging.adapter.inbound.repository.EmailRepository;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -25,22 +27,26 @@ public class EmailService {
     private String emailFrom;
 
     @Transactional
-    public EmailEntity sendEmail(EmailEntity email) {
+    public void sendEmail(EmailEntity email) {
         try {
             email.setSendDateEmail(LocalDateTime.now(ZoneId.of("America/Sao_Paulo")));
             email.setEmailFrom(emailFrom);
 
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(email.getEmailTo());
-            message.setSubject(email.getSubject());
-            message.setText(email.getText());
+            MimeMessage message = emailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, false, "utf-8");
+
+            helper.setTo(email.getEmailTo());
+            helper.setSubject(email.getSubject());
+            helper.setFrom(emailFrom);
+
+            helper.setText(email.getText(), true);
 
             emailSender.send(message);
             email.setStatus(Status.SENT);
-        } catch (MailException e) {
+        } catch (MailException | MessagingException e) {
             email.setStatus(Status.ERROR);
         } finally {
-            return emailRepository.save(email);
+            emailRepository.save(email);
         }
     }
 
